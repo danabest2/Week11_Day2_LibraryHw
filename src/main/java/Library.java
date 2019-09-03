66b7fbdfc965533b9c4f95258c6fc7a4c7976c29
DANIELA <danielamiritescu@gmail.com>
2019-09-03 17:49:13 +0100

    checked capacity

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,19 +2,38 @@ import java.util.ArrayList;
 
 public class Library {
 
+
     private String name;
     private ArrayList<Book> collection;
+    private int capacity;
 
-    public Library(String name) {
+    public Library(String name, int capacity) {
         this.name = name;
         this.collection = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
     public int collectionSize(){
         return this.collection.size();
     }
 
+    public int collectionCount() {
+        return this.collection.size();
+    }
+
     public void add(Book book) {
         this.collection.add(book);
     }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    //public int capacitySize(){
+        //return this.capacity.size();
+    //}
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,15 +8,32 @@ public class LibraryTest {
     Library library;
     Book book;
 
+
+
     @Before
     public void setUp() {
-        library = new Library("Central");
+        library = new Library("Central", 4);
         book = new Book("","", "");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Central", library.getName());
+    }
+
+    @Test
+    public void startsWithEmptyCollection(){
+        assertEquals(0, library.collectionCount());
     }
 
     @Test
     public void canAddToCollection() {
         library.add(book);
         assertEquals( 1, library.collectionSize());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(4,library.getCapacity());
     }
 }
